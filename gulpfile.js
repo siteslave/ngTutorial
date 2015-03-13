77e87b21ae1e70c07b132ed759d8df61bc148cc7
Satit Rianpit <rianpit@gmail.com>
2015-03-13 12:25:45 +0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    jade = require('gulp-jade'),
+    jshint = require('gulp-jshint'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less');
+
+/** Jade **/
+gulp.task('jade', function () {
+    return gulp.src('./src/**/*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('./app'));
+});
+
+/** JSHint **/
+gulp.task('jshint', function () {
+    return gulp.src(['./src/**/*.js', './src/app.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(gulp.dest('./app'));
+});
+/** LESS **/
+gulp.task('less', function () {
+    return gulp.src('./src/**/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./app'));
+});
+
+/** Watch **/
+gulp.task('watch', function () {
+    gulp.watch('./src/**/*.js', ['jshint']);
+    gulp.watch('./src/**/*.jade', ['jade']);
+    gulp.watch('./src/**/*.less', ['less']);
+});
+
+/** Default task **/
+gulp.task('default', ['jshint', 'jade', 'less', 'watch']);
